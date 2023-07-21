507d12d017ce4e24f924e8b89327589e7ff71e39
raiduzhniy <138212236+raiduzhniy@users.noreply.github.com>
2023-07-21 09:21:58 +0300

    about us page

--- src/app/modules/basic/pages/about-us/about-us.component.ts
+++ src/app/modules/basic/pages/about-us/about-us.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { LoaderDirective } from '@shared/directives';
+import { AboutUsFacade } from '@state/about-us';
 
 @Component({
   selector: 'rdn-about-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, LoaderDirective],
+  providers: [AboutUsFacade],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
-export class AboutUsComponent {}
+export class AboutUsComponent implements OnInit {
+  isLoading$ = this.aboutUsFacade.isLoading$;
+  aboutUs$ = this.aboutUsFacade.aboutUs$;
+  constructor(private aboutUsFacade: AboutUsFacade) {}
+
+  ngOnInit() {
+    this.aboutUsFacade.dispatchGetAboutUs();
+  }
+}
